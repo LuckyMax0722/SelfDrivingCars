5daf65a10bc62da2169049f1b6b1d97a7a8cfa0b
jiachen <jiachen_lu1999@163.com>
2023-11-01 20:19:32 +0100

    commit_231101

--- lib/file_name_process.py
+++ lib/file_name_process.py
@@ -1,8 +1,8 @@
 import csv
 
 # 读取原始CSV文件
-input_file = '/home/jiachen/SelfDrivingCars/data_D/driving_log.csv'
-output_file = '/home/jiachen/SelfDrivingCars/data_D/re.csv'
+input_file = '/home/jiachen/SelfDrivingCars/data/driving_log.csv'
+output_file = '/home/jiachen/SelfDrivingCars/data/re.csv'
 
 with open(input_file, 'r') as input_csv, open(output_file, 'w', newline='') as output_csv:
     csv_reader = csv.reader(input_csv)
@@ -11,15 +11,9 @@ with open(input_file, 'r') as input_csv, open(output_file, 'w', newline='') as o
     for row in csv_reader:
         # 检查是否有足够的列数（至少3列）
         if len(row) >= 3:
-            # 在第一列的字符串前添加'/home/jiachen/SelfDrivingCars/data/'
-            row[0] = f'/home/jiachen/SelfDrivingCars/data/{row[0]}'
-            # 删除第二和第三列的字符串的第一个字符
-            row[1] = row[1][1:]
-            row[1] = f'/home/jiachen/SelfDrivingCars/data/{row[1]}'
-            row[1] = f' {row[1]}'
-            row[2] = row[2][1:]
-            row[2] = f'/home/jiachen/SelfDrivingCars/data/{row[2]}'
-            row[2] = f' {row[2]}'
+            row[0] = row[0].replace('/home/jiachen', '/home/tum')
+            row[1] = row[1].replace(' /home/jiachen', ' /home/tum')
+            row[2] = row[2].replace(' /home/jiachen', ' /home/tum')
 
         csv_writer.writerow(row)
 
